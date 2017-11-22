ce2e8b3a094e30a52822415802f22322e7b7bbaa
HankelBao <hankelbao@outlook.com>
2017-11-23 00:00:42 +0800

    Add some other files that I forgot to stage

---
+++ lisp/init-ivy.el
@@ -0,0 +1,4 @@
+(require-packages '(ivy counsel-projectile))
+(ivy-mode 1)
+(counsel-projectile-on)
+(provide 'init-ivy)
---
+++ lisp/init-workgroups2.el
@@ -0,0 +1,6 @@
+(require-packages '(workgroups2))
+(require 'workgroups2)
+(setq wg-session-file "~/.emacs.d/.emacs_workgroups")
+(workgroups-mode 1)
+
+(provide 'init-workgroups2)
