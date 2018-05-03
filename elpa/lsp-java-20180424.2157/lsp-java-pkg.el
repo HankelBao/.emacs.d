a8ed5932f168205665200380834c09877116371a
HankelBao <hankelbao@outlook.com>
2018-05-03 21:15:01 +0800

    sync

---
+++ elpa/lsp-java-20180424.2157/lsp-java-pkg.el
@@ -0,0 +1,2 @@
+;;; -*- no-byte-compile: t -*-
+(define-package "lsp-java" "20180424.2157" "Java support for lsp-mode" '((emacs "25.1") (lsp-mode "3.0")) :commit "2b5970fca944c046bd9be02c38699fa9e397b4f0" :keywords '("java") :url "https://github.com/emacs-lsp/lsp-java")
